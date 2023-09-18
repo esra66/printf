dfa11ded44c6d601b3210ba7e997cbfb5e02aace
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-18 14:54:10 -0700

    putchar edit

--- putchar.c
+++ putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
+
 /**
  * _putchar - Writes a character to stdout
  * @c: The character to write
  * Return: 1 on success, -1 on failure
  */
-#define BUFFER_SIZE 1024
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
