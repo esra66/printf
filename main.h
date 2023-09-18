dce32e7db55daff895810723f459d7b890d86fb5
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-18 10:59:01 -0700

    main header

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+
+typedef int (*FormatFunction)(va_list);
+/**
+ * struct FormatSpecifier - define the type of foramt
+ * @specifier: char represent the format
+ * @function: function handle specifier
+ */
+struct FormatSpecifier
+{
+	const char specifier;
+	FormatFunction function;
+};
+
+int _printf(const char *format, ...);
+int _strlen(char *s);
+int _putchar(char c);
+int _print_char(va_list args);
+int _print_string(va_list args);
+int _print_int(va_list args);
+int _print_deci(va_list args);
+int _print_oct(va_list args);
+int _print_lowhex(va_list args);
+int _print_unisg(va_list args);
+int _print_upperhex(va_list args);
+int _print_perc(va_list args);
+#endif
+
