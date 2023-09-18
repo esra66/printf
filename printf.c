8c441e3251d44ae5352e21953a4eaca1de906545
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-18 12:36:50 -0700

    printf function

---
+++ printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _printf - cuztomied printf function
+ * @format: format string
+ * Return: number of char been printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	int i = 0;
+	unsigned int j = 0;
+	va_list args;
+
+	FormatSpecifier s[] = {
+		{'c', _print_char}, {'s', _print_string}, {'d', _print_deci},
+		{'i', _print_int}, {'x', _print_lowhex}, {'X', _print_upperhex},
+		{'o', _print_oct}, {'%', _print_perc}, {'u', _print_unisg},
+	};
+
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	for (i = 0 ; format[i] != '\0' ; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	else
+	{
+		i++;
+		for (j = 0 ; j < sizeof(s) / sizeof(s[0]) ; j++)
+		{
+			if (s[j].specifier == format[i])
+			{
+				count += s[j].function(args);
+				break;
+			}
+		}
+	}
+}
+	va_end(args);
+	return (count);
+}
